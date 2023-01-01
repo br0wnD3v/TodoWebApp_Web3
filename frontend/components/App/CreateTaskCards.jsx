603a5bf54e345e158f5ee4a4a06d3b05c17fc621
br0wnD3v <bothereum@protonmail.com>
2023-01-02 04:43:18 +0530

    Little text

--- frontend/components/App/CreateTaskCards.jsx
+++ frontend/components/App/CreateTaskCards.jsx
@@ -4,6 +4,10 @@ export default function CreateTaskCards(props) {
   var elements = [];
   const mode = props.mode;
 
+  // 0 - Serial Number
+  // 1 - Task
+  // 2 - Timestamp
+  // 3 - Status
   if (mode == 0) {
     for (var index = 0; index < props.data.length; index++) {
       if (!props.data[index][2].isZero())
